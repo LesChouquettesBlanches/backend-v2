26a9a23b37f28b8abdd8d5b1ae9bfce79a52845e
Nicolas <anicolas.business@gmail.com>
2023-03-06 19:57:49 +0100

    Initial commit

---
+++ app.ts
@@ -0,0 +1,29 @@
+require('dotenv').config()
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+/*const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
+const orderRoutes = require('./routes/order')
+const bookerRoutes = require('./routes/booker')
+const staffRoutes = require('./routes/staff')*/
+
+mongoose
+  .connect(process.env.DATABASE_URI!)
+  .then(() => console.log('Connexion à MongoDB réussie !!'))
+  .catch((e) => console.log(e, 'Connexion à MongoDB échouée !'))
+
+/*app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/order', orderRoutes)
+app.use('/api/booker', bookerRoutes)
+app.use('/api/staff', staffRoutes)*/
+
+export default app
